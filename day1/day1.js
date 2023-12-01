48cb29441cef81b41a3c4a151f48818f6d6e55cd
Derek Tran <tran.derek.d@gmail.com>
2023-12-01 12:32:20 -0800

    Solve part 1

---
+++ day1/day1.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt').toString('utf-8').split('\n');
+
+let sum = 0;
+input.forEach((line) => {
+  // Remove all non-numeric characters
+  let num = line.replace(/\D/g, '');
+  
+  // Grabs the first and last digit, if there is only one digit it duplicates it
+  num = num[0] + num[num.length - 1];
+  sum += +num;
+});
+
+console.log(sum);
